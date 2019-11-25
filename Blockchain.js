f65afb4d887852a13d0115f0aca675e6a1f7d64c
Rajat Gupta <7rajatgupta@gmail.com>
2019-11-25 15:42:31 +0530

    ready-arch

--- Blockchain.js
+++ Blockchain.js
@@ -12,4 +12,12 @@ class Blockchain {
   getLatestBlock() {
     return this.chain[this.chain.length - 1];
   }
+
+  addNewBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
 }
+
+module.exports = Blockchain;
---
+++ main.js
@@ -0,0 +1,49 @@
+const Block = require("./Block");
+const Blockchain = require("./Blockchain");
+
+//Init
+let bankChain = new Blockchain();
+
+//Add Blocks:
+bankChain.addNewBlock(
+  new Block(1, "20/12/2019", {
+    account_id: "IK7YHVBGTYJKI78",
+    current_amount: 233234,
+    currency: "INR",
+    bank_name: "State Bank Of India",
+    username: "Mr. Bot1",
+    last_transaction: "74820246492",
+    has_savings: true,
+    account_verified: true,
+    active_loan: false
+  })
+);
+
+bankChain.addNewBlock(
+  new Block(2, "22/12/2019", {
+    account_id: "IK8YHVBGTYJKI78",
+    current_amount: 78644,
+    currency: "INR",
+    bank_name: "HDFC Bank",
+    username: "Mr. Bot2",
+    last_transaction: "4484546492",
+    has_savings: true,
+    account_verified: true,
+    active_loan: true
+  })
+);
+bankChain.addNewBlock(
+  new Block(3, "24/12/2019", {
+    account_id: "IUH76TVBGTYJKI78",
+    current_amount: 4523654,
+    currency: "INR",
+    bank_name: "Bank of Baroda",
+    username: "Mr. Bot3",
+    last_transaction: "56214246492",
+    has_savings: false,
+    account_verified: true,
+    active_loan: true
+  })
+);
+
+console.log(JSON.stringify(bankChain, null, 4));
